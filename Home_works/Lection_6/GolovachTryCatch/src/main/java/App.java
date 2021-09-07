8973b60ba7eeb1752debac7eefcbf278eaac7e91
Ruslan Huseinov <50515791+RuslanHuseinov@users.noreply.github.com>
2021-09-07 12:03:23 +0300

    area

--- Home_works/Lection_6/GolovachTryCatch/src/main/java/App.java
+++ Home_works/Lection_6/GolovachTryCatch/src/main/java/App.java
@@ -1,4 +1,18 @@
+import java.io.IOException;
+
 public class App {
+    public static void main(String[] args) {
+        System.out.println("hello error");
+        String str = infinity()==null?"":infinity();
+        System.out.println(str);
+    }
+    public static int area(int width, int height){
+        if (width < 0 || height < 0){
+            throw new IllegalArgumentException("Negative sizes: w = " + width + ", h = " + height);
+        }
+        return height * width;
+    }
+
     public static void foo(NullPointerException exc){
         try {
             throw exc;
@@ -6,7 +20,12 @@ public class App {
             foo(sameExc);
         }
     }
-    public static void main(String[] args) {
-        foo(null);
+
+    public static String infinity() /*throws Throwable*/{
+//        throw new IOException();
+//        throw new ArithmeticException();
+        throw new NullPointerException();
+//        System.exit(0);
+//        throw new Error();
     }
 }
