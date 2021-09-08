0c445a3ef2e197643161cda52456f8ffad209a88
Ruslan Huseinov <50515791+RuslanHuseinov@users.noreply.github.com>
2021-09-08 12:25:00 +0300

    nested

--- Home_works/Lection_6/GolovachTryCatch/src/main/java/App.java
+++ Home_works/Lection_6/GolovachTryCatch/src/main/java/App.java
@@ -1,42 +1,24 @@
-import java.io.IOException;
 
 public class App {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         try {
-            Throwable exc = new Error();
-            throw  exc;
-        }catch (RuntimeException exc){
-            System.err.println("runtime catched");
-        }catch (Exception exc){
-            System.err.println("exception catched");
-        }catch (Throwable exc){
-            System.err.println("throwable catched");
+            System.err.print(" 0");
+            try {
+                System.err.print(" 1");
+                if (true) {throw new RuntimeException();}
+                System.err.print(" 2");
+            } catch (RuntimeException e) {
+                System.err.print(" 3");
+            } finally {
+                System.err.print(" 4");
+            }
+            System.err.print(" 5");    
+        } catch (Exception e) {
+            System.err.print(" 6");
+        } finally {
+            System.err.print(" 7");
         }
-
-
-
-
-
-
-//        try {
-//            System.err.print(" 0");
-//            if (true) {throw new RuntimeException();}
-//            System.err.print(" 1");
-//        } catch (RuntimeException e) {
-//            System.err.print(" 2");
-//            try {
-//                System.err.print(" 3");
-//                if (true) {throw new Error();}
-//                System.err.print(" 4");
-//            } catch (Throwable t) {
-//                System.err.print(" 5");
-//            }
-//            System.err.print(" 6");
-//        } catch (Error e) {
-//            System.err.print(" 7");
-//        }
-//        System.err.println(" 8");
-
+        System.err.print(" 8");
     }
-    // 0 2 3 5 6 8
+
 }
