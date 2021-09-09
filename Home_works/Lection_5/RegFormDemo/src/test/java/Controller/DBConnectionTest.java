a857202f41e16bd6f09aabcd26e915ff4a3272be
Ruslan Huseinov <50515791+RuslanHuseinov@users.noreply.github.com>
2021-09-09 14:29:17 +0300

    fix test

--- Home_works/Lection_5/RegFormDemo/src/test/java/Controller/DBConnectionTest.java
+++ Home_works/Lection_5/RegFormDemo/src/test/java/Controller/DBConnectionTest.java
@@ -1,6 +1,7 @@
 package Controller;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
 
 import java.sql.*;
 
@@ -9,15 +10,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class DBConnectionTest {
     @Test
     void isConnectionWork (){
-        Connection connection = DBConnection.DB_CONNECTION_INSTANCE.getConnection();
-        try (Statement statement = connection.createStatement()) {
-            ResultSet resultSet = statement.executeQuery("SELECT * FROM users");
-            while (resultSet.next()){
-                System.out.println(resultSet.getString(1));
-            }
-        }catch (SQLException exc){
-            System.err.println(exc);
-        }
+        assertDoesNotThrow(DBConnection.DB_CONNECTION_INSTANCE::getConnection);
 
     }
 }
